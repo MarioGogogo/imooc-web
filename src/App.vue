b3c02425b32376fee6759306b4550b0d29055d38
EdisonChan <148373644@qq.com>
2022-05-23 17:44:17 +0800

    init：项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { isMobileTerminal } from './utils/flexible.js';
+</script>
+
+<template>
+  <div>是否移动设备---{{ isMobileTerminal }}</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/utils/flexible.js
@@ -0,0 +1,11 @@
+/**
+ * 屏幕宽度是否大于指定宽度 1280
+ */
+import { computed } from 'vue'
+import { PC_DEVICE_WIDTH } from '../constants/index'
+import { useWindowSize } from '@vueuse/core'
+// 响应式
+const { width } = useWindowSize()
+export const isMobileTerminal = computed(() => {
+  return width.value < PC_DEVICE_WIDTH
+})
