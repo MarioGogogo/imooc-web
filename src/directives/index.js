e15f2ecb396dd3b8179090e1bf6985d5a1776a2d
EdisonChan <148373644@qq.com>
2022-05-30 19:27:11 +0800

    feat:指令自动注册

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,7 +1,12 @@
-import lazyImg from './modules/lazyImg'
+import lazyImg from './modules/lazy'
 
 export default {
   install(app) {
-    app.directive('lazy', lazyImg)
+    const directives = import.meta.globEager('./modules/*.js')
+    for (const [key, value] of Object.entries(directives)) {
+      const arr = key.split('/')
+      const directivesName = arr[arr.length - 1].replace('.js', '')
+      app.directive(directivesName, value.default)
+    }
   },
 }
