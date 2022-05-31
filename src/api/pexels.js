60d7626128b32c03a4848cae960752e81fc84ff5
EdisonChan <148373644@qq.com>
2022-05-31 23:28:54 +0800

    feat:热门精选api

--- src/api/pexels.js
+++ src/api/pexels.js
@@ -1,17 +1,25 @@
 import request from '@/utils/request'
 
+// 获取壁纸
 export const getPexlesList = (data) => {
   return request({
     url: '/pexels',
     params: data,
   })
 }
-
+// 获取搜索标签
 export const getHintsList = (q) => {
   return request({
     url: '/search',
     params: {
       q,
     },
+  })
+}
+
+// 热门精选
+export const getHotTheme = () => {
+  return request({
+    url: '/theme',
   })
 }
